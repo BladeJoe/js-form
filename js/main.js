8a187abf329406114489ae47c4265732a7c5878b
Joe <106337992+BladeJoe@users.noreply.github.com>
2022-07-02 21:22:01 +0500

    new

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log("hi");
+ 
+var name = prompt("Whats your name?")
+var surname = prompt("Whats your surname?")
+var age = prompt("Whats your age?")
+var study = prompt("Where do you study?")
+var hobby = prompt("Whats your hobby?")
+var englishLevel = prompt("Whats your english level?")
+
+console.log(`Name:${name}\nSurname:${surname}\nAge:${age}\nStudy:${study}\nHobby:${hobby}\nEnglish level:${englishLevel}`);
